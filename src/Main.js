e3c851467034af5b7b16ee7e17c88bdf88434433
rajeshavul2 <rajeshavul2@gmail.com>
2023-11-20 13:28:16 +0200

    lab done

---
+++ src/Main.js
@@ -0,0 +1,19 @@
+// MainContent.js
+import React from 'react';
+
+function MainContent() {
+  return (
+    <div>  
+      <section>
+    <h2>Section 1</h2>
+    <p>This is the content of section 1.</p>
+  </section>
+  <section>
+    <h2>Section 2</h2>
+    <p>This is the content of section 2.</p>
+  </section>
+  </div>
+  );
+}
+
+export default MainContent;
